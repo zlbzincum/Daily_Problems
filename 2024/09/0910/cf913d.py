b7df831bae8fc901c4372fe632d1e32023d57c84
“zlbzincum” <zlb189079606@163.com>
2024-09-12 11:07:19 +0800

    240912 cf1267e C++

--- 2024/09/0910/cf913d.py
+++ 2024/09/0910/cf913d.py
@@ -1,8 +1,15 @@
+from heapq import merge, heapify, heappush, heappop # heapq 是小根堆
+
+fmax = lambda x, y: x if x > y else y
+fmin = lambda x, y: x if x < y else y
+
 def MII():
+    import io, os, sys
+    input = lambda: sys.stdin.readline().strip()
     return map(int, input().split())
 
 def main():
-    n,t = MII()
+    n, t = MII()
     vals = []
     ts = []
 
@@ -11,12 +18,28 @@ def main():
         vals.append(a)
         ts.append(p)
 
-        st_range = list(range(n))
-        st_range.sort(key=lambda x: -vals[x])
+    st_range = list(range(n))
+    st_range.sort(key=lambda x: -vals[x])
 
-        ans = 0
-        hpd = []
-        cur = 0
-        for i in st_range:
-            heappush(hpd, -ts[i])
-            cur
+    ans = 0
+    hpq = []
+    cur = 0
+    for i in st_range:
+        heappush(hpq, -ts[i])
+        cur += ts[i]
+        if cur > t:
+            cur += heappop(hpq)
+        ans = fmax(ans, fmin(vals[i], len(hpq)))
+
+    print(ans)
+
+    st_range.sort(key=lambda x: ts[x])
+    to_choose = []
+    for i in st_range:
+        if vals[i] >= ans and len(to_choose) < ans:
+            to_choose.append(i + 1)
+
+    print(len(to_choose))
+    print(' '.join(map(str, to_choose)))
+
+main()
